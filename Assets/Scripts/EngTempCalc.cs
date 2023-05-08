e316a65d85242779801bea3ab91181e3ed3a5a27
Bibhor2000 <108837222+Bibhor2000@users.noreply.github.com>
2023-05-08 13:49:47 -0400

    Initial Commit
    The first commit

---
+++ Assets/Scripts/EngTempCalc.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EngTempCalc : MonoBehaviour
+{
+    float randomNumber ;
+    public TextMeshProUGUI text;
+
+    void Start()
+    {
+        InvokeRepeating("Calculate", 1.25f, 1.25f);
+    }
+    // Update is called once per frame
+    void Calculate()
+    {
+        randomNumber = Random.Range(0, 250); 
+        text = GetComponent<TextMeshProUGUI> ();
+        text.text = randomNumber.ToString();  
+    }
+    
+}
---
+++ Assets/Scripts/ObjectNex.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "ObjectNex", menuName = "ScriptableObjects/ObjectNex")]
+public class ObjectNex : ScriptableObject
+{
+     
+}
